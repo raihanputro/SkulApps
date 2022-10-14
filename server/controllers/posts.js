72e55d8ba9c65d8c2e25b8720d583e92a52aa51a
raihanputro <raihanputromaulana477@gmail.com>
2022-10-14 08:52:30 +0700

    Adding Like Post Feature

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -46,3 +46,14 @@ export const deletePost = async (req, res) => {
     
     res.json(deletedPost);
 }
+
+export const likePost = async (req, res) => {
+    const {id: _id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Tidak ada post dengan id tersebut!');
+
+    const post = await PostMessage.findById(_id);
+    const likedPost = await PostMessage.findByIdAndUpdate(_id, { likeCount: post.likeCount + 1}, { new: true });
+
+    res.json(likedPost);
+}
