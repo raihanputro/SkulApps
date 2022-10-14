0e6aaef0006542fa403b1628ff9c5eb111020d0b
raihanputro <raihanputromaulana477@gmail.com>
2022-10-14 09:31:20 +0700

    Adding Env

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotEnv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotEnv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -13,10 +15,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://raihanputro:raihanputro040601@skulapps.0qtjmkp.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server berjalan pada port: ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server berjalan pada port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} tidak terhubung!`));
 
