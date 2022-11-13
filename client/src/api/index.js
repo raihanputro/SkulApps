33809340d693e59dd2b1c2bf3cf72c49d5a2f5a8
raihanputro <raihanputromaulana477@gmail.com>
2022-11-13 19:50:00 +0700

    Cleaning code

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/schools';
 
-export const fetchSchools = () => axios.get(url);
-export const createSchool = (newSchool) => axios.post(url, newSchool);
-export const updateSchool = (id, updatedSchool) => axios.patch(`${url}/${id}`, updatedSchool);
-export const deleteSchool = (id) => axios.delete(`${url}/${id}`);
-export const likeSchool = (id) => axios.patch(`${url}/${id}/likeSchool`);
+const urlSchools = 'http://localhost:5000/schools';
+
+export const fetchSchools = () => axios.get(urlSchools);
+export const createSchool = (newSchool) => axios.post(urlSchools, newSchool);
+export const updateSchool = (id, updatedSchool) => axios.patch(`${urlSchools}/${id}`, updatedSchool);
+export const deleteSchool = (id) => axios.delete(`${urlSchools}/${id}`);
+export const likeSchool = (id) => axios.patch(`${urlSchools}/${id}/likeSchool`);
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
@@ -12,10 +12,11 @@ document.getElementById('root').style.fontFamily = "Roboto, sans-serif";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(
+const root  = createRoot(document.getElementById('root'));
+
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
-);
+    </Provider>
+)
 
