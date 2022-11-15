784a656557ab1af965d3c22316a9f93b2973cd0b
raihanputro <raihanputromaulana477@gmail.com>
2022-11-16 00:53:02 +0700

    Creating school details  feature

---
+++ client/src/components/posts/postDetails/postDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useStyles from "./styles";
+
+const SchoolDetails = () => {
+    const classes = useStyles();
+    console.log("DETAIL SEKOLAH");
+
+    return (
+        <>
+            DETAIL SEKOLAH
+        </>
+    )
+}
+
+export default SchoolDetails;
