0c4dc1f19f9bfc63b5a813a31623efaf7c143fdf
raihanputro <raihanputromaulana477@gmail.com>
2022-11-16 18:50:56 +0700

    Updating code in navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -36,14 +36,14 @@ const Navbar = () => {
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+                <Link to="/" className={classes.heading} variant="h2" align="center">
                     SkulApps
-                </Typography>
+                </Link>
             </div>
             <Toolbar className={classes.toolbar}> 
                 {user ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.picture}>
+                        <Avatar className={classes.purple} alt={user?.result.name} src={user?.result?.picture || user?.result?.profilePic}>
                             {user?.result.name.charAt(0)} 
                         </Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
